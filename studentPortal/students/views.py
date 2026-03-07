f6346b241a989ab2841c355130df39a9521833b3
Arham Khan <mdarhamkhan22@gmail.com>
2026-03-07 14:51:00 +0530

    Created views and models

--- studentPortal/students/views.py
+++ studentPortal/students/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from students.models import TableOne
 
-# Create your views here.
+def  home(request):
+    user_data = TableOne.objects.all() # SELECT * FROM TableOne
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        mobile = request.POST.get('mobile')
+        email = request.POST.get('email')
+        
+        file = request.FILES.get('file')
+        
+        TableOne.objects.create(name= name, mobile= mobile, email= email, file= file)
+        return redirect("/")
+
+    return render(request, 'home.html', {'data': user_data})
